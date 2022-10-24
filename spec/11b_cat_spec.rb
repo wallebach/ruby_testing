c2f0667e6ea32beb6d63cbf297d5cde23db1ea22
wallebach <114591465+wallebach@users.noreply.github.com>
2022-10-24 15:08:11 +0200

    Implement spec #11

--- spec/11b_cat_spec.rb
+++ spec/11b_cat_spec.rb
@@ -35,17 +35,20 @@ end
 
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
-
+  subject(:mustache) { described_class.new('Mustache', 'British Shorthair') }
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
-  context '' do
+  context 'when Cat responds to talk' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
+  it 'is not hungry' do
+    expect(subject).not_to be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(subject).to be_hiding
   end
 end
